3560b189d88ed6a4f6da8a9853ecd2a5a6a90885
Merlijn-S <97105788@st.deltion.nl>
2025-03-12 15:20:23 +0100

    Klaar

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,13 @@
 # Opdracht 1 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+print(joe+bob+fred)
+
+print((joe+bob+fred) / 3)
