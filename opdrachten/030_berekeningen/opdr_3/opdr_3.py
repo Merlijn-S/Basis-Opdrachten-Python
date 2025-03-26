b4c6d6ae57df8b01b014a60e886bbb823dc4279e
Merlijn-S <97105788@st.deltion.nl>
2025-03-26 14:47:15 +0100

    klaar

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -1,16 +1,17 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code...
+
 x = 1
-y = ...
-print( ... )
+y = 4*(x**3) - 2*(x**2) + 1
+print("De uitkomst is:", y)
 
 x = 2
-y = ...
-print( ... )
+y = 4*(x**3) - 2*(x**2) + 1
+print("De uitkomst is:", y)
 
 x = 0
-y = ...
-print( ... )
+y = 4*(x**3) - 2*(x**2) + 1
+print("De uitkomst is:", y)
