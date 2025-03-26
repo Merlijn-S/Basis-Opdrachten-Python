b3f2b77faff4ae3c4308410b7449cc75d6950d5c
Merlijn-S <97105788@st.deltion.nl>
2025-03-26 14:57:55 +0100

    klaar

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,19 @@
 # Opdracht 1 lists
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 mylist = ...
 dict_1 = ...
 dict_2 = ...
 dict_3 = ...
 dict_4 = ...
+
+
+lijst = [
+    {"id": 0, "voornaam": "Poep", "achternaam": "Slaaf"},
+    {"id": 1, "voornaam": "Pluk", "achternaam": "Slaaf"},
+    {"id": 2, "voornaam": "Katoen", "achternaam": "Plukker"},
+    {"id": 3, "voornaam": "Anne", "achternaam": "Frank"}
+]
+
+print(lijst[1]["voornaam"], lijst[1]["achternaam"])
