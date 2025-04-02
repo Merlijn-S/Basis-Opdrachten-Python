6694b86ac73b6acd53b074723d02b3b8cff69288
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 15:01:56 +0200

    Klaar

--- opdrachten/060_loops/opdr_5/opdr_4.py
+++ opdrachten/060_loops/opdr_5/opdr_4.py
@@ -1,9 +1,10 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+getallen = [x * 3 for x in range(1, 21)]  
+print(getallen[:6])  
