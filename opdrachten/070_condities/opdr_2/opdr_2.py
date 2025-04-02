ea7fcd326196e5b7df7437f5528a604fdf1b7afe
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 15:06:29 +0200

    Klaar

--- opdrachten/070_condities/opdr_2/opdr_2.py
+++ opdrachten/070_condities/opdr_2/opdr_2.py
@@ -1,6 +1,6 @@
 # Opdracht 2 condities
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code...
 
@@ -11,3 +11,9 @@ my_list = [43948, 878768, 38768, 87555, 765765]
 # for...:
 #   if...:
 #       print()
+
+getallen = [43948, 878768, 38768, 87555, 765765]
+
+for getal in getallen:
+    if getal % 3 == 0: 
+        print(getal, end=" ")
