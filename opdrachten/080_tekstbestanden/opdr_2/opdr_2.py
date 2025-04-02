6819de00d48e20e51db9df7ea3b48beba69622ec
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 15:18:28 +0200

    Klaar

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -1,6 +1,6 @@
 # Opdracht 2 tekstbestanden
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 import random
 prompt = "Raad mijn geheime getal \n"
@@ -8,3 +8,20 @@ geheim_getal = random.randint(1, 100)
 
 # De rest moet jij doen.....
 
+import random
+
+geheim_getal = random.randint(1, 100)
+aantal_pogingen = 0
+
+while True:
+    gok = int(input("Raad mijn geheime getal\n"))
+    aantal_pogingen += 1
+    
+    if gok == geheim_getal:
+        print(f"Je hebt het getal geraden het is {geheim_getal}!")
+        print(f"Je hebt het in {aantal_pogingen} keer gedaan")
+        break
+    elif gok < geheim_getal:
+        print("hoger")
+    else:
+        print("lager")
