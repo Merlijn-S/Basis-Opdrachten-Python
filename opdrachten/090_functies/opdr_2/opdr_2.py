643c59446e0be0b0915ef3678e02c6c7ae34a0af
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 15:28:49 +0200

    Klaar

--- opdrachten/090_functies/opdr_2/opdr_2.py
+++ opdrachten/090_functies/opdr_2/opdr_2.py
@@ -1,20 +1,36 @@
 # Opdracht 1 functies
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
-
-def kilometers_naar_miles(km):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+def kilometers_naar_miles(kilometers):
+    """
+    Converteert kilometers naar miles (1 mile = 1.609344 km)
+    
+    Args:
+        kilometers (float): Aantal kilometers om te converteren
+        
+    Returns:
+        float: Het equivalente aantal miles
+    """
+    return kilometers / 1.609344
 
 def miles_naar_kilometers(miles):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """
+    Converteert miles naar kilometers (1 mile = 1.609344 km)
+    
+    Args:
+        miles (float): Aantal miles om te converteren
+        
+    Returns:
+        float: Het equivalente aantal kilometers
+    """
+    return miles * 1.609344
 
 kilometers = 1223
 miles = 867
 
-print(kilometers_naar_miles(kilometers))
-print(miles_naar_kilometers(miles))
+miles_resultaat = kilometers_naar_miles(kilometers)
+km_resultaat = miles_naar_kilometers(miles)
+
+print(f"{kilometers} kilometers = {miles_resultaat} miles")
+print(f"{miles} miles = {km_resultaat} kilometers")
