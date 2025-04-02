af38f3b0ed247a4a2b5361285b1dfd07af61829e
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 15:00:18 +0200

    Klaar

--- opdrachten/060_loops/opdr_4/opdr_4.py
+++ opdrachten/060_loops/opdr_4/opdr_4.py
@@ -1,9 +1,8 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code...
 
-# Hier start de for-loop
-
-my_list = []
+y_waarden = [4*x + 7 for x in range(1, 11)]  
+print(y_waarden) 
