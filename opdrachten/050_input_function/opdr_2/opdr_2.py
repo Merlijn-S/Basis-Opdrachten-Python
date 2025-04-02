93d8c1ac33729f262ea41e95a740ed9604d23d57
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 14:29:12 +0200

    Klaar

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -1,7 +1,17 @@
 # Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+gasten = ["Paul", "Kees", "Marie", "Hilda"]
+
+gasten.insert(0, "Merlijn")
+print("Originele lijst:", gasten) # ["Merlijn", "Paul", "Kees", "Marie", "Hilda"]
+
+gasten.remove("Marie")
+print("Na afzegging Marie:", gasten)  # ["Merlijn", "Paul", "Kees", "Hilda"]
+
+kees_index = gasten.index("Kees")  # Vind de positie van Kees
+gasten.insert(kees_index + 1, "George")  # Voeg George toe na Kees
+print("Met George erbij:", gasten)  # ["Merlijn", "Paul", "Kees", "George", "Hilda"]
