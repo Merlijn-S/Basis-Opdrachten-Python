38611ed54d65b12d5d5ce079262d8fc2c3d16bcc
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 14:40:19 +0200

    Klaar

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -1,6 +1,21 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code...
 
+objecten = []
+print("Voer minimaal 5 steden in (druk na elke stad op Enter):")
+for i in range(5):
+    invoer = input(f"Voer stad {i+1} in: ")
+    objecten.append(invoer)
+
+extra = input("Wil je nog een stad toevoegen? (ja/nee): ")
+while extra.lower() == "ja":
+    invoer = input("Voer extra stad in: ")
+    objecten.append(invoer)
+    extra = input("Wil je nog een stad toevoegen? (ja/nee): ")
+
+objecten.sort(reverse=True)
+
+print("Gesorteerde lijst in omgekeerde volgorde:", objecten)
