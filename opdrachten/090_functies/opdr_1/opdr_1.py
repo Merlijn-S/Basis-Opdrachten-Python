9a30ee8b4dce2f9b783ca9e16292c10a8df7deee
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 15:29:49 +0200

    Klaar

--- opdrachten/090_functies/opdr_1/opdr_1.py
+++ opdrachten/090_functies/opdr_1/opdr_1.py
@@ -3,15 +3,6 @@
 # Groep: IT2B
 
 
-def write_to_file(afile, atext):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
-
-my_tekst = "Schrijf dit maar even in een bestandje"
-my_file = "test.txt"
-write_to_file(my_file, my_tekst)
-
 def write_to_file(bestandsnaam, tekst):
     """
     Schrijft tekst naar een bestand. Als het bestand al bestaat, wordt de tekst toegevoegd.
