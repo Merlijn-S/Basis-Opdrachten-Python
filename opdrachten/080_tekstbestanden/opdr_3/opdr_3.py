c9223028c0024729561db23ae54bb076b8abcc5f
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 15:21:42 +0200

    Klaar

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -1,6 +1,28 @@
 # Opdracht 3 Tekst opslaan
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
+def encrypt_tekst(tekst):
+    versleutelde_tekst = ""
+    
+    for letter in tekst:
+        if letter.isalpha():
+            is_hoofdletter = letter.isupper()
+            code = ord(letter.lower())
+            nieuwe_code = code + 5
+            if nieuwe_code > ord('z'):
+                nieuwe_code -= 26
+            nieuwe_letter = chr(nieuwe_code)
+            if is_hoofdletter:
+                nieuwe_letter = nieuwe_letter.upper()
+            versleutelde_tekst += nieuwe_letter
+        else:
+            versleutelde_tekst += letter
+            
+    return versleutelde_tekst
 
+invoer = input("Geef de tekst die wilt encrypten..\n")
+
+resultaat = encrypt_tekst(invoer)
+print(resultaat)
 
