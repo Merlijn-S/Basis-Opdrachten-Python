53ae857d8f1e8a949fbe41a1803129c96258c109
Merlijn-S <97105788@st.deltion.nl>
2025-04-02 14:15:26 +0200

    klaar

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -1,7 +1,17 @@
 # Opdracht 1 input function
-# Naam student:
-# Groep:
+# Naam student: Merlijn Schaap
+# Groep: IT2B
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+import math
 
+def pythag (a, b)
+        sum_of_squares = a * a + b * b
+        c = math.sqrt(sum_of_squares)
+
+        return c
+
+solution = pythag(4, 3)
+
+print(solution)
